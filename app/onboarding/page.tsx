402e610e3f3b6d76c13a74e9eb62345333a1bb33
Jitendra <jituvasishta0110@gmail.com>
2024-08-19 15:42:32 -0700

    Created onboarding page

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { OnboardLanding } from "@/components/onboard-landing";
+
+const Onboarding = () => {
+  return (
+    <OnboardLanding></OnboardLanding>
+  )
+}
+
+export default Onboarding;
